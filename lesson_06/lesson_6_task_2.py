6b3fbfa16719d5f0a5119f5cb3d6e3ab8f715b40
ArkaKor <tor784@yande.ru>
2025-08-08 00:18:06 +0300

    lesson_6

---
+++ lesson_06/lesson_6_task_2.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/textinput")
+search_box = driver.find_element(By.CSS_SELECTOR, "#newButtonName")
+search_box.send_keys("SkyPro")
+driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+result = driver.find_element(By.CSS_SELECTOR, "#updatingButton").text
+print(result)
+driver.quit()
